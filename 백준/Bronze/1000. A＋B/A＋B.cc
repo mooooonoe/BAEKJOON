fb404431e47f01ee4c8e5b23753bf2bce506d1af
MOONSUBIN <92616429+mooooonoe@users.noreply.github.com>
2023-06-27 14:19:43 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+
+	int A = 0;
+	int B = 0;
+	//printf("input A is ");
+	scanf("%d", &A);
+	//printf("\ninput B is ");
+	scanf("%d", &B);
+	
+	printf("%d", A + B);
+
+
+}
