9bbee3a2f9c24dfa181c3621a9afbf10bdcab330
MOONSUBIN <92616429+mooooonoe@users.noreply.github.com>
2023-06-30 00:57:05 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int num = 0;	//시험 본 과목 개수
+	scanf("%d", &num);
+
+	int* arr = (int*)malloc(sizeof(int) * num);
+	for (int i = 0; i < num; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	int max = 0;	//최댓값
+	int pos = 0;
+	for (int i = 0; i < num; i++) {
+		if (max < arr[i]) {
+			max = arr[i];
+			pos = i;
+		}
+	}
+
+	float sum = 0;
+	/*for (int i = 0; i < num; i++) {
+		//printf("%d %d %f", arr[i], arr[pos], (float)arr[i]/(float)arr[pos]);
+		if (i == pos) {
+			sum = sum + (float)arr[i];
+			continue;
+		}
+		else {
+			float one = 0;
+			one = (float)arr[i] / max;
+			arr[i] = (one * 100);
+		}
+
+		sum = sum + (float)arr[i];
+	}*/
+
+	for (int i = 0; i < num; i++) {
+		sum += (float)arr[i] / max * 100;
+	}
+
+	printf("%f", sum / num);
+	
+
+	return 0;
+}
