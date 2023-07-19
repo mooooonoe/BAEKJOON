123c17583dd280109d8ca405d890fa10680d4cde
MOONSUBIN <92616429+mooooonoe@users.noreply.github.com>
2023-07-19 23:36:59 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int dp[11] = { 0, };
+
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+
+	for (int i = 4; i < 11; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+	}
+
+	int T, num;
+	scanf("%d", &T);
+
+	for (int i = 0; i < T; i++) {
+		scanf("%d", &num);
+		printf("%d\n", dp[num]);
+	}
+	
+	return 0;
+}
