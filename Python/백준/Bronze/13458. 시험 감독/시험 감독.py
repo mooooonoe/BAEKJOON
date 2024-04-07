900efea496a691e0915d615d74d4199346e4c06e
MOONSUBIN <92616429+mooooonoe@users.noreply.github.com>
2024-04-08 01:59:39 +0900

    [Bronze II] Title: 시험 감독, Time: 828 ms, Memory: 144340 KB -BaekjoonHub

---
+++ Python/백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -0,0 +1,15 @@
+N = int(input())
+tester = list(map(int, input().split()))
+a,b = map(int, input().split())
+
+total = 0
+for i in range(N):
+    tester[i] -= a
+    total += 1
+    if tester[i] > 0:
+        if tester[i]%b == 0:
+            total += (tester[i])//b
+        else:
+            total += tester[i]//b+1
+
+print(total)
