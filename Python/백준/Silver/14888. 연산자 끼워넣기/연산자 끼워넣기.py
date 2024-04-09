963912a4ad054cb4c11956cf23459dbe05de736f
MOONSUBIN <92616429+mooooonoe@users.noreply.github.com>
2024-04-09 12:39:29 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 156 ms, Memory: 34188 KB -BaekjoonHub

--- Python/백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ Python/백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,7 +1,7 @@
 N = int(input())
 num = list(map(int, input().split()))
-operators = list(map(int, input().split()))  # 각 연산자 개수
-dtool = [0, 1, 2, 3]  # +, -, *, /
+operators = list(map(int, input().split()))
+dtool = [0, 1, 2, 3] # +  -  *  /
 
 def permutation(arr, r):
     result = []
@@ -19,37 +19,33 @@ def permutation(arr, r):
     gen_permutation(arr, r)
     return result
 
-# 모든 연산자 순열 구하기
 org = []
 for i in range(4):
-    org.extend([dtool[i]] * operators[i])
+    org.extend([dtool[i]] * operators[i]) ####
 
 Ori = list(permutation(org, N-1))
 
-def calculate_expression(a, b, op):
+def Calculate(a,b,op):
     if op == 0:
-        return a + b
+        return a+b
     elif op == 1:
-        return a - b
+        return a-b
     elif op == 2:
-        return a * b
+        return a*b
     elif op == 3:
-        # 음수를 양수로 나눌 때는 C++14의 기준을 따름
         if a < 0:
-            return -((-a) // b)
+            return -(abs(a)//b)       #### 정수 나눗셈 ***
         else:
-            return a // b
+            return a//b
 
-# 초기값 설정
 min_result = float('inf')
-max_result = float('-inf')
+max_result = float('-inf') #### 최대최소 초기값 설정
 
-# 모든 연산자 순열에 대해 계산
-for ops in Ori:
+for ops in Ori: #### 반복문 이렇게 써서 calculate 할때 ops 를 가져다 씀 !!! 왕 !!
     result = num[0]
-    for i in range(N-1):
-        result = calculate_expression(result, num[i+1], ops[i])
-    
+    for i in range(N - 1):
+        result = Calculate(result, num[i + 1], ops[i])
+
     min_result = min(min_result, result)
     max_result = max(max_result, result)
 
