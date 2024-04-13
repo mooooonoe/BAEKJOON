a701e002398db7aa2c1e94c16edd14f25a38c691
MOONSUBIN <92616429+mooooonoe@users.noreply.github.com>
2024-04-14 00:22:24 +0900

    [Bronze II] Title: 시험 감독, Time: 596 ms, Memory: 143316 KB -BaekjoonHub

--- Python/백준/Bronze/13458. 시험 감독/시험 감독.py
+++ Python/백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -1,15 +1,15 @@
 N = int(input())
+
 tester = list(map(int, input().split()))
 a,b = map(int, input().split())
 
 cnt = N
-
 for i in tester:
     i -= a
-    if i > 0:
-        if i % b:
+    if i > 0 :
+        if i%b:
             cnt += (i//b)+1
         else:
-            cnt += (i//b)
+            cnt += i//b
 
 print(cnt)
