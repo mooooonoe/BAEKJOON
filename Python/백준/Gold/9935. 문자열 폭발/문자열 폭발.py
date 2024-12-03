6e68aebd993a272498fa6a5962aceb68d8aaeb6a
MOONSUBIN <92616429+mooooonoe@users.noreply.github.com>
2024-12-03 14:00:03 +0900

    [Gold IV] Title: 문자열 폭발, Time: 532 ms, Memory: 41268 KB -BaekjoonHub

--- Python/백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ Python/백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,19 +1,22 @@
-arr = input()
-bumb = input()
+def check(ori, boom):
+    stack = []
+    boom_len = len(boom)
 
-stack = []
-bumb_len = len(bumb)
+    for char in ori:
+        stack.append(char)
 
-for elem in arr:
-    stack.append(elem)
-    # 스택의 마지막 문자가 bumb와 일치하는지 확인
-    if len(stack) >= bumb_len and ''.join(stack[-bumb_len:]) == bumb:
-        # 스택에서 bumb만큼 제거
-        del stack[-bumb_len:]
+        if ''.join(stack[-boom_len:]) == boom:
+            del stack[-boom_len:]
+        
+    return ''.join(stack)
 
-# 스택이 비어 있으면 "FRULA" 출력
-if not stack:
-    print("FRULA")
+
+ori = input()
+boom = input()
+
+result = check(ori, boom)
+
+if result:
+    print(result)
 else:
-    # 스택에 남아있는 문자들을 합쳐서 출력
-    print(''.join(stack))
+    print("FRULA")
