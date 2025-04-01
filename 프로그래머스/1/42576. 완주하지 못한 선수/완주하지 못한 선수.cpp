725b2e1af88751f01dce7674bf3fede4911d3950
MOONSUBIN <92616429+mooooonoe@users.noreply.github.com>
2025-04-02 00:36:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 41.86 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    
+    unordered_map<string, int> m;
+    
+    for(string name : participant){
+        m[name]++;
+    }
+    
+    for(string name : completion){
+        m[name]--;
+    }
+    
+    for(auto entry : m){
+        if(entry.second > 0){
+            return entry.first;
+        }
+    }
+    
+    
+    
+    return "";
+}
