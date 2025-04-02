f04730b4778214caa38b77431535abfde95a5e48
MOONSUBIN <92616429+mooooonoe@users.noreply.github.com>
2025-04-02 11:34:41 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 
 using namespace std;
-//i부터 j 까지 정렬 했을 때 K 번째에 있는 수 return 
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
@@ -13,11 +12,10 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
         int j = cmd[1];
         int k = cmd[2];
         
-        vector<int> temp(array.begin()+i-1, array.begin()+j);
+        vector<int> tmp(array.begin()+i-1, array.begin()+j);
+        sort(tmp.begin(), tmp.end());
+        answer.push_back(tmp[k-1]);
         
-        sort(temp.begin(), temp.end());
-        
-        answer.push_back(temp[k-1]);
     }
     
     return answer;
