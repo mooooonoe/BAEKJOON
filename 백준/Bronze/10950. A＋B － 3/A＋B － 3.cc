30b9dc1740c35bcb2e1bb38bd7ff8a21930ff59b
MOONSUBIN <92616429+mooooonoe@users.noreply.github.com>
2025-04-03 15:46:18 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>  
+using namespace std;
+
+// 테스트케이스 개수
+// 각 한줄씩 .. 
+
+int main(void) {
+    int tc,a,b = 0;
+    cin >> tc;
+
+
+    while(tc--){
+        cin >> a >>b;
+        cout << a+b << '\n'; 
+    }
+
+    return 0;
+}
