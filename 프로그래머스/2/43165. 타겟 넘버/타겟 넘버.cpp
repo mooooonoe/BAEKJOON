5202ee5f139aba37bfd60c52521b1bb039b36592
MOONSUBIN <92616429+mooooonoe@users.noreply.github.com>
2025-04-03 14:23:52 +0900

    [level 2] Title: 타겟 넘버, Time: 6.16 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int cnt=0;
+
+void DFS(vector<int> &numbers, int target, int L,int sum){
+    
+    if(L==numbers.size()){
+        if(sum==target) cnt++;
+    }else{
+        DFS(numbers,target,L+1,sum+numbers[L]);
+        DFS(numbers,target,L+1,sum-numbers[L]);
+    }
+    
+}
+
+int solution(vector<int> numbers, int target) {
+            
+    DFS(numbers,target,0,0);
+    
+    return cnt;
+}
