4d4239adf24e7323966802524ab1124c98b8efa5
MOONSUBIN <92616429+mooooonoe@users.noreply.github.com>
2025-04-05 18:33:52 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -1,58 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-// 올림픽 실버 5
-// 금 수 / 금 같으면 은 수 / 금 은 수 같으면 동
-// 한 국가의 등수는 자신보다 잘한 나라 수 + 1
-// 금은동 모두 같으면 두 나라 등수 같음
+struct Country {
+    int id;
+    int gold;
+    int silver;
+    int bronze;
+};
 
-int main(void){
-
-    // n 국가 수 // k 등수 알고 싶은 국가 
-
-    int n, k = 0;
+int main() {
+    int n, k;
     cin >> n >> k;
 
-    // n 개의 줄동안 각 국가 정수, 금, 은, 동
-
-    // vector <vector<int>> v(0, vector<int> (0));
-    vector <vector<int>> v(n, vector<int>(4));
-
-    // for(int i=0; i<4; i++){
-    //     for(int j=0l j<n; j++){
-            
-    //     }
-    // }
-    for(int i=0; i<n; i++){
-        cin >> v[i][0] >> v[i][1] >> v[i][2] >> v[i][3];
+    vector<Country> countries(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> countries[i].id >> countries[i].gold >> countries[i].silver >> countries[i].bronze;
     }
 
+    // 메달 기준으로 정렬
+    sort(countries.begin(), countries.end(), [](const Country& a, const Country& b) {
+        if (a.gold != b.gold) return a.gold > b.gold;
+        if (a.silver != b.silver) return a.silver > b.silver;
+        return a.bronze > b.bronze;
+    });
 
-    int targetGold, targetSilver, targetBronze;
-    
-    for(int i=0; i<n; i++){
-        if(v[i][0] == k){
-            targetGold = v[i][1];
-            targetSilver = v[i][2];
-            targetBronze = v[i][3];
-            break;
+    // 국가 번호별 등수 저장
+    vector<int> rank(n + 1);  // 국가 번호는 1부터 시작
+    int currentRank = 1;
+    rank[countries[0].id] = currentRank;
+
+    for (int i = 1; i < n; i++) {
+        if (countries[i].gold == countries[i - 1].gold &&
+            countries[i].silver == countries[i - 1].silver &&
+            countries[i].bronze == countries[i - 1].bronze) {
+            rank[countries[i].id] = currentRank; // 동점자 처리
+        } else {
+            currentRank = i + 1;
+            rank[countries[i].id] = currentRank;
         }
     }
 
-    int rank = 1;
-    
-    for(int i=0; i<n; i++){
-        int g = v[i][1];
-        int s = v[i][2];
-        int b = v[1][3];
-
-        if(g>targetGold) rank++;
-        else if(g==targetGold && s>targetSilver) rank++;
-        else if(g==targetGold && g==targetSilver && b>targetBronze) rank++;
-    }
-
-    cout << rank << '\n';
+    cout << rank[k] << '\n';
     return 0;
 }
